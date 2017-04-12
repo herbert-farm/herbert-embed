83947ac80980c5249401c9e605bfe7407f84ffb0
joshpaulchan <joshpaulchan@gmail.com>
2017-04-12 03:19:40 -0400

    Add networking config

---
+++ config.py
@@ -0,0 +1,45 @@
+"""
+@name   Config
+@desc   File for containing the program configuration
+"""
+
+class Config(Config):
+    """Global Config"""
+    
+    pass
+
+class NetworkConfig(Config):
+    """Networking configuration"""
+    
+    HOST = 'localhost'
+    PORT = 50007
+    ENCODING = 'utf-8'
+    
+    MAX_THREADS = 5
+
+class GPIOConfig(Config):
+    """GPIO configuration"""
+    
+    PINS = {
+        "lights/enable"     : 2,
+        "lights/red"        : None,
+        "lights/green"      : None,
+        "lights/blue"       : None,
+        "sensors/enable"    : 3,
+        "pump/enable"       : 5,
+        "fan/enable"        : 6
+    }
+    
+    FRIENDLY_NAMES = {out:name for name, out in PINS.items() if out}
+
+class CLI_Config(Config):
+    """CLI configuration"""
+    
+    CMD = {
+        'LIST'  : 0,
+        'BSET'  : 1,
+        'HELP'  : 2,
+        'EXIT'  : 7
+    }
+    
+    PS2 = "[gpio] "
