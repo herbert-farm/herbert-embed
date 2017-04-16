b956da5dae1816cc51d05c5ed7d1303ad046ca48
joshpaulchan <joshpaulchan@gmail.com>
2017-04-16 01:35:10 -0400

    Add logging & debug config, switch to ordered dicts to maintain name-pin structure

--- embed/config.py
+++ embed/config.py
@@ -3,12 +3,18 @@
 @desc   File for containing the program configuration
 """
 
+import logging
 from collections import OrderedDict
 
 class Config(object):
     """Global Config"""
     
     DEBUG = True
+    
+    # loggin
+    LOG_FILE = "herbert.log"
+    LOG_LEVEL = logging.INFO
+    LOG_FORMAT = '%(levelname)s:%(message)s'
 
 class NetworkConfig(Config):
     """Networking configuration"""
@@ -22,6 +28,7 @@ class NetworkConfig(Config):
 class GPIOConfig(Config):
     """GPIO configuration"""
     
+    # TODO: change from PINS to PIN_NUMBERS and FRIENDLY_NAMES to PIN_NAME
     PINS = OrderedDict({
         "lights/enable"     : 2,
         "lights/red"        : None,
@@ -31,17 +38,17 @@ class GPIOConfig(Config):
         "pump/enable"       : 5,
         "fan/enable"        : 6
     })
+    FRIENDLY_NAMES = {out:name for name, out in PINS.items() if out is not None}
     
-    FRIENDLY_NAMES = {out:name for name, out in PINS.items() if out}
+    CHNL_NUMBERS = OrderedDict({
+        "temp/front"        : 0,
+        "temp/middle"       : 1
+    })
+    CHNL_NAMES = {out:name for name, out in CHNL_NUMBERS.items() if out is not None}
+    
+    FILENAME = 'gpio_states.json'    # filename to load/store state from
 
 class CLI_Config(Config):
     """CLI configuration"""
-    
-    CMD = {
-        'LIST'  : 0,
-        'BSET'  : 1,
-        'HELP'  : 2,
-        'EXIT'  : 7
-    }
     
     PS2 = "[gpio] "
