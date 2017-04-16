dbbd1bab5d8337ef732f18025810070bd9434b73
joshpaulchan <joshpaulchan@gmail.com>
2017-04-16 02:03:21 -0400

    Add (commented-out) basic implementation of new variable message socket

--- embed/gpio/sock.py
+++ embed/gpio/sock.py
@@ -6,6 +6,8 @@ Builds upon the socket class to allow easy parsing of variable-length messages d
 
 import socket
 
+from embed import config
+
 class Socket(socket.socket):
     """
     `Socket`
@@ -28,3 +30,38 @@ class Socket(socket.socket):
         
         # Align stream and start reading untilJ
         return super(Socket, self).recv(1024, *args, **kwargs)
+
+
+# class Socket(object):
+#     """demonstration class only
+#       - coded for clarity, not efficiency
+#     """
+# 
+#     def __init__(self, sock=None):
+#         if sock is None:
+#             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#             self.encoding = config.NetworkConfig.ENCODING
+#         else:
+#             self.sock = sock
+# 
+#     def connect(self, host, port):
+#         self.sock.connect((host, port))
+# 
+#     def mysend(self, msg):
+#         totalsent = 0
+#         while totalsent < MSGLEN:
+#             sent = self.sock.send(msg[totalsent:])
+#             if sent == 0:
+#                 raise RuntimeError("socket connection broken")
+#             totalsent = totalsent + sent
+# 
+#     def myreceive(self):
+#         chunks = []
+#         bytes_recd = 0
+#         while bytes_recd < MSGLEN:
+#             chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+#             if chunk == b'':
+#                 raise RuntimeError("socket connection broken")
+#             chunks.append(chunk)
+#             bytes_recd = bytes_recd + len(chunk)
+#         return b''.join(chunks)
