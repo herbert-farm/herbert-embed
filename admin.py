3cf6c8f0904b85068cfc8884ed8ad1004d169ca3
joshpaulchan <joshpaulchan@gmail.com>
2017-04-16 01:27:59 -0400

    Update admin code

--- admin.py
+++ admin.py
@@ -5,14 +5,14 @@
 @author Joshua Paul A. Chan (@joshpaulchan)
 """
 
-import curses
-
 from embed.gpio import Client
 from embed import config, utils
 
 PINS = config.GPIOConfig.PINS.values()
-NAMES = config.GPIOConfig.FRIENDLY_NAMES
-VALID_COMMANDS = set(['BSET', 'LIST', 'HELP', 'EXIT'])
+CHNLS = config.GPIOConfig.CHNL_NUMBERS.values()
+PIN_NAMES = config.GPIOConfig.FRIENDLY_NAMES
+CHNL_NAMES = config.GPIOConfig.CHNL_NAMES
+VALID_COMMANDS = set(['SET', 'GET', 'LIST', 'HELP', 'EXIT'])
 
 @utils.assert_to_false
 def valid_cmd(chunks):
@@ -24,47 +24,67 @@ def valid_cmd(chunks):
     # must be one of the commands listed in COMMANDS
     pcmd = chunks[0]
     
-    print(pcmd)
-    if utils.caseless_compare(pcmd, 'BSET'):
+    if utils.caseless_compare(pcmd, 'SET'):
         # validator for 'SET'
         # must have at least 2 arguments
         assert len(chunks) >= 3
-        # both must be numeric
+        # pin must be numeric
         assert chunks[1].isnumeric() and chunks[2].isnumeric()
         # pin number must be within range
         assert int(chunks[1]) in PINS
-        return True
+    elif utils.caseless_compare(pcmd, 'GET'):
+        # validator for 'GET'
+        # must have at least 2 arguments (pin or channel) and specifier
+        assert len(chunks) >= 3
+        
+        # first arg (pin/channel) must be alpha, and either 'p' or 'c'
+        assert chunks[1].isalpha() and chunks[1] in ('p'.casefold(), 'c'.casefold())
+        
+        # pin/channel specifier must be numeric
+        assert chunks[2].isnumeric()
+        if utils.caseless_compare(chunks[1], 'p'):
+            # pin number must be within range
+            assert int(chunks[1]) in PINS
+        elif utils.caseless_compare(chunks[1], 'c'):
+            # channel number must be within range
+            assert int(chunks[1]) in CHNLS
     elif utils.caseless_compare(pcmd, 'LIST'):
         # validator for 'LIST'
-        # no args needed
-        return True
+        # must have at least 1 arg
+        assert len(chunks) >= 2
+        
+        # arg (pin/channel) must be alpha, and either 'p' or 'c'
+        assert chunks[1].isalpha() and chunks[1] in ('p'.casefold(), 'c'.casefold())
     elif utils.caseless_compare(pcmd, 'HELP'):
         # validator for 'HELP'
-        return True
+        # no args needed
+        pass
     elif utils.caseless_compare(pcmd, 'EXIT'):
         # validator for  'EXIT'
-        return True
-    elif pcmd in VALID_COMMANDS:
+        # no args needed
+        pass
+    elif pcmd not in VALID_COMMANDS:
         # validate all other commands in VALID_COMMANDS
-        return True
-    return False
+        return False
+    return True
 
 def parse_cmd(chunks):
     """Return action from type of command."""
     cmd = chunks[0]
     action = {"type" : cmd}
     
-    if utils.caseless_compare(cmd, 'BSET'):
+    if utils.caseless_compare(cmd, 'SET'):
+        # parse pin and val
         action['pin'] = int(chunks[1])
         action['val'] = int(chunks[2])
-    
+    elif utils.caseless_compare(cmd, 'LIST'):
+        # parse which to list, 'p' for pins or 'c' for channels
+        action['which'] = chunks[1]
     return action
     
 # main
 def main():
-    """
-    """
-    # TODO: create the screens
+    """main boilerplate"""
     # initialize client
     ps2 = config.CLI_Config.PS2
     client = Client()
@@ -79,25 +99,41 @@ def main():
             continue
             
         action = parse_cmd(pcmd)
-        if utils.caseless_compare(action['type'], 'BSET'):
+        act_type = action['type']
+        
+        if utils.caseless_compare(act_type, 'SET'):
             pin = action['pin']
             val = action['val']
             
             try:
-                res = client.set(pin, val)
-                print("'{}' (pin #{}) is now: {}".format(NAMES[pin], pin, val))
+                res = client.set_pin(pin, val)
+                print("'{}' (pin #{}) is now: {}".format(PIN_NAMES[pin], pin, val))
             except:
                 print("Error setting pin #{} to {}.".format(pin, val))
-        elif utils.caseless_compare(action['type'], 'LIST'):
-            res = client.get_all()
+        elif utils.caseless_compare(act_type, 'LIST'):
+            which = action['which']
             
-            for pin, val in res['pins'].items():
-                print("{:20} (pin #{:0>2}): {}".format(NAMES[int(pin)], pin, val))
+            if which == 'p':
+                res = client.get_pins()
                 
-        elif utils.caseless_compare(action['type'], 'HELP'):
+                pins = res['pins']
+                for pin, val in pins.items():
+                    name = PIN_NAMES[int(pin)]
+                    print("{:20} (pin #{:0>2}): {}".format(name, pin, val))
+            elif which == 'c':
+                res = client.get_channels()
+                
+                channels = res['channels']
+                for channel, val in channels.items():
+                    name = CHNL_NAMES[int(channel)]
+                    print("{:20} (pin #{:0>2}): {}".format(name, channel, val))
+            else:
+                print("No such listing `{}` available.".format(which))
+                
+        elif utils.caseless_compare(act_type, 'HELP'):
             print("Valid commands: " + ','.join(map("'{}'".format, VALID_COMMANDS)))
                 
-        elif utils.caseless_compare(action['type'], 'EXIT'):
+        elif utils.caseless_compare(act_type, 'EXIT'):
             # let em off easy
             print("Have a nice day!")
             exit()
